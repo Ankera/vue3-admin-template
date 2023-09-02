d916b3748e4f90b56c5d790875b405c95c54d786
ankera <15189120919@163.com>
2023-09-02 17:32:38 +0800

    feat: 测试格式化

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 <!-- 模块主要作用:  -->
 
 <template>
-  <div>
+  <div class="box">
     <h1>hello</h1>
   </div>
 </template>
@@ -16,4 +16,8 @@ const num = ref(0)
 console.log(num.value)
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.box {
+  width: 100px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,10 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 // var a = 1;
+
+const func = () => 
+{
+    console.log(11)
+}
+
+func()
