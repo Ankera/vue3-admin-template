372b0c36fcbec28f9c6290dec700343f081527c6
ankera <15189120919@163.com>
2023-09-02 15:57:25 +0800

    feat: init vue3-admin

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- 作者: 余亚勇 -->
+<!-- 创建时间: 2023-09-02 15:54:35 -->
+<!-- 模块主要作用:  -->
+
+<template>
+  <div>
+    <h1>hello</h1>
+  </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style scoped>
+
+</style>
