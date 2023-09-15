1a121cddc61f64842bbcddead97cfebdf85ed200
ankera <15189120919@163.com>
2023-09-15 17:13:08 +0800

    feat: 抽离第三方打包

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ import pinia from '@/store/index'
 import { isHasButton } from '@/directive/has'
 
 // 路由守卫
-import './permission'
+// import './permission'
 
 // 测试mock数据
 // import axios from 'axios'
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { ConfigEnv, UserConfigExport, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
@@ -6,12 +6,22 @@ import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 import { viteMockServe } from 'vite-plugin-mock'
+import commonjs from 'rollup-plugin-commonjs'
+import externalGlobals from 'rollup-plugin-external-globals'
+
+const globals: any = externalGlobals({
+  vue: 'Vue',
+  // vuex: 'Vuex',
+  // vueRouter: 'VueRouter',
+  'element-plus': 'ElementPlus',
+  'vue-demi': 'VueDemi',
+})
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default ({ command, mode }: ConfigEnv): UserConfigExport => {
   // 获取各种环境下对应的变量
   const env = loadEnv(mode, process.cwd())
-  // console.log('env', env)
+  console.log('启动命令参数集合===NEW===', command, mode, env)
   return {
     plugins: [
       vue(),
@@ -39,8 +49,11 @@ export default defineConfig(({ command, mode }) => {
       },
     },
     build: {
+      target: 'es2015',
+      // assetsDir: './static',
       rollupOptions: {
-        external: ['element-plus', 'vue'], // 将 element-plus, vue 标记为外部依赖
+        external: ['element-plus', 'vue', 'vue-demi'], // 将 element-plus, vue 标记为外部依赖
+        plugins: [commonjs(), globals],
       },
     },
     // 代理跨域
@@ -57,4 +70,4 @@ export default defineConfig(({ command, mode }) => {
       },
     },
   }
-})
+}
